59bdd34cae02ac5f3933089ad2e25059a049fc44
Neill Robson <neill@neillrobson.com>
2025-06-06 13:59:31 -0400

    Finish v1 of info page
    Closes GH-15.

--- App/Bunnydex/InfoView.swift
+++ App/Bunnydex/InfoView.swift
@@ -7,12 +7,24 @@
 
 import SwiftUI
 
+let infoText = """
+                [Killer Bunnies](https://killerbunnies.com/) is a registered trademark of Jeffrey Bellinger. All rights reserved. All copyrights and trademarks used under license.
+
+                This fan-made iOS application is designed and developed by [Neill Robson](https://neillrobson.com).
+
+                Inspired by [Bunnypedia](https://play.google.com/store/apps/details?id=com.fueledbycaffeine.bunnypedia&hl=en_US), designed and developed by [Josh Friend](https://github.com/joshfriend/bunnypedia).
+
+                App icon art by Jennifer Robson.
+                """
+
 struct InfoView: View {
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(.init(infoText))
+                .multilineTextAlignment(.center)
+                .padding()
                 .navigationTitle("About")
                 .toolbar {
                     ToolbarItem {
