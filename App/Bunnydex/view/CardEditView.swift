a535669b16b8c9a2f7e353330a3cf0fc24ec39a8
Neill Robson <neill@neillrobson.com>
2025-07-18 15:06:14 -0400

    GH-35 Add Pawn selector

--- App/Bunnydex/view/CardEditView.swift
+++ App/Bunnydex/view/CardEditView.swift
@@ -13,27 +13,38 @@ struct CardEditView: View {
 
     var body: some View {
         Form {
-            LabeledContent("ID") {
-                TextField("0000", text: $card.id)
-                    .multilineTextAlignment(.trailing)
-            }
-            LabeledContent("Title") {
-                TextField("Choose A Carrot", text: $card.title)
-                    .multilineTextAlignment(.trailing)
-            }
-            Picker("Deck", selection: $card.rawDeck) {
-                ForEach(Deck.allCases) { deck in
-                    Text(deck.description.display).tag(deck.rawValue)
+            Section("Fundamentals") {
+                LabeledContent("ID") {
+                    TextField("0000", text: $card.id)
+                        .multilineTextAlignment(.trailing)
+                }
+                LabeledContent("Title") {
+                    TextField("Choose A Carrot", text: $card.title)
+                        .multilineTextAlignment(.trailing)
+                }
+                Picker("Deck", selection: $card.rawDeck) {
+                    ForEach(Deck.allCases) { deck in
+                        Text(deck.description.display).tag(deck.rawValue)
+                    }
+                }
+                Picker("Card type", selection: $card.rawType) {
+                    ForEach(CardType.allCases) { type in
+                        Text(type.description.display).tag(type.rawValue)
+                    }
+                }
+                Picker("Bunny requirement", selection: $card.rawRequirement) {
+                    ForEach(BunnyRequirement.allCases) { requirement in
+                        Text(requirement.description.display).tag(requirement.rawValue)
+                    }
                 }
             }
-            Picker("Card type", selection: $card.rawType) {
-                ForEach(CardType.allCases) { type in
-                    Text(type.description.display).tag(type.rawValue)
-                }
-            }
-            Picker("Bunny requirement", selection: $card.rawRequirement) {
-                ForEach(BunnyRequirement.allCases) { requirement in
-                    Text(requirement.description.display).tag(requirement.rawValue)
+
+            Section("Optionals") {
+                Picker("Pawn", selection: $card.rawPawn) {
+                    Text("None").tag(Int?(nil))
+                    ForEach(Pawn.allCases) { pawn in
+                        Text(pawn.description.display).tag(pawn.rawValue)
+                    }
                 }
             }
         }
