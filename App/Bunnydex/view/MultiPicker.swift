31ebc20ad48e4c7e1713d6c5bf843ac557312d81
Neill Robson <neill@neillrobson.com>
2025-07-18 15:06:28 -0400

    GH-35 Create MultiPicker util

---
+++ App/Bunnydex/view/MultiPicker.swift
@@ -0,0 +1,81 @@
+//
+//  MultiPicker.swift
+//  Bunnydex
+//
+//  Created by Neill Robson on 7/18/25.
+//
+
+import SwiftUI
+
+struct MultiPicker<LabelView: View, Selectable: Identifiable & Hashable>: View {
+    let label: LabelView
+    let options: [Selectable]
+    let optionToString: (Selectable) -> String
+    var selected: Binding<Set<Selectable>>
+
+    private var formattedSelectedListString: String {
+        ListFormatter.localizedString(byJoining: selected.wrappedValue.map(optionToString))
+    }
+
+    var body: some View {
+        NavigationLink(destination: multiSelectionView()) {
+            HStack {
+                label
+                Spacer()
+                Text(formattedSelectedListString)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.trailing)
+            }
+        }
+    }
+
+    private func multiSelectionView() -> some View {
+        MultiPickerView(options: options, optionToString: optionToString, selected: selected)
+    }
+}
+
+struct MultiPickerView<Selectable: Identifiable & Hashable>: View {
+    let options: [Selectable]
+    let optionToString: (Selectable) -> String
+
+    @Binding var selected: Set<Selectable>
+
+    var body: some View {
+        List {
+            ForEach(options) { selectable in
+                Button(action: { toggleSelection(selectable: selectable) }) {
+                    HStack {
+                        Text(optionToString(selectable)).foregroundColor(.primary)
+                        Spacer()
+                        if selected.contains(selectable) {
+                            Image(systemName: "checkmark").foregroundColor(.accentColor)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private func toggleSelection(selectable: Selectable) {
+        if let existingIndex = selected.firstIndex(where: { $0.id == selectable.id }) {
+            selected.remove(at: existingIndex)
+        } else {
+            selected.insert(selectable)
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var selectedDice: Set<Die> = [.red, .blue, .chineseZodiac]
+
+    NavigationView {
+        Form {
+            MultiPicker<Text, Die>(
+                label: Text("Dice"),
+                options: Die.allCases,
+                optionToString: \.description.display,
+                selected: $selectedDice
+            )
+        }
+    }
+}
